5a40c8a9290b2ddbe9652d8117364f0f386d7e8b
David Christensen <david.christensen@paymentspring.com>
2021-05-10 15:01:21 -0500

    Adds capture_timeout_warning ctrl param to class method

--- sentry-ruby/lib/sentry/lambda.rb
+++ sentry-ruby/lib/sentry/lambda.rb
@@ -2,8 +2,8 @@ require 'sentry/lambda/capture_exceptions'
 
 module Sentry
   module Lambda
-    def self.capture_exceptions(event, context)
-      CaptureExceptions.new(event, context).call do
+    def self.capture_exceptions(event, context, catpure_timeout_warning = false)
+      CaptureExceptions.new(event, context, catpure_timeout_warning).call do
         yield
       end
     end
