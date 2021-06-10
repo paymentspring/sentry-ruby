97eb8e0f0d8877ea560b5d7238918e0eac2b033f
David Christensen <david.christensen@paymentspring.com>
2021-06-10 09:24:22 -0500

    Adds NullContext object to help with testing in other projects

--- sentry-lambda/lib/sentry/lambda.rb
+++ sentry-lambda/lib/sentry/lambda.rb
@@ -1,16 +1,17 @@
 require "sentry-ruby"
 require "sentry/integrable"
 require "sentry/lambda/capture_exceptions"
+require "sentry/lambda/null_context"
 
 module Sentry
   module Lambda
     extend Integrable
     register_integration name: 'lambda', version: Sentry::Lambda::VERSION
 
-    def self.wrap_handler(event:, context:, capture_timeout_warning: false)
+    def self.wrap_handler(event:, context: NullContext.new, capture_timeout_warning: false)
       CaptureExceptions.new(
         aws_event: event,
-        aws_context: context,
+        aws_context: context || NullContext.new,
         capture_timeout_warning: capture_timeout_warning
       ).call do
         yield
